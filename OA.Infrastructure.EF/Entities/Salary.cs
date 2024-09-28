d576aa74e84c86c282fe2eafe572b9f8743f73d4
vanphong19 <phongdv1901@gmail.com>
2024-09-28 20:35:27 +0700

    salary

---
+++ OA.Infrastructure.EF/Entities/Salary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.Infrastructure.EF.Entities
+{
+    public class Salary : BaseEntity
+    {
+        public string UserId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public double PITax { get; set; }
+
+        public double TotalSalary {  get; set; }
+    }
+}

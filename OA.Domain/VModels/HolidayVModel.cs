ec643c07a9e95cca864c50cc6d118abad752aad9
vanphong19 <phongdv1901@gmail.com>
2024-10-20 16:00:06 +0700

    holiday

---
+++ OA.Domain/VModels/HolidayVModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.Core.VModels
+{
+    public class HolidayCreateVModel
+    {
+
+    }
+    public class HolidayVModel
+    {
+    }
+}
---
+++ OA.Infrastructure.EF/Entities/Holiday.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.Infrastructure.EF.Entities
+{
+    public class Holiday : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Note { get; set; } = string.Empty;
+    }
+}

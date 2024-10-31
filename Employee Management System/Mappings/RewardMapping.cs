c1fae1f88ebee3cafaa5485dea8c1073254edb94
MinhNoProUIT <154415039+MinhNoProUIT@users.noreply.github.com>
2024-10-31 18:29:56 +0700

    update benefit

---
+++ Employee Management System/Mappings/RewardMapping.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using OA.Core.VModels;
+using OA.Domain.VModels;
+using OA.Infrastructure.EF.Entities;
+
+namespace OA.WebAPI.Mappings
+{
+    public class RewardMapping : Profile
+    {
+        public RewardMapping() 
+        {
+            //Insert
+            CreateMap<RewardCreateVModel, Reward>();
+            // Update
+            CreateMap<RewardUpdateVModel, Reward>();
+            //Get All 
+            CreateMap<Reward, RewardGetAllVModel>();
+            //Get By Id
+            CreateMap<Reward, RewardGetByIdVModel>();
+            //Get list
+            CreateMap<Reward, RewardExportVModel>();
+        }
+    }
+    }
+
---
+++ OA.Domain/Services/IRewardService.cs
@@ -0,0 +1,15 @@
+﻿using OA.Core.Models;
+using OA.Core.Services;
+using OA.Core.VModels;
+using OA.Domain.VModels;
+using OA.Infrastructure.EF.Entities;
+using System.Threading.Tasks;
+
+namespace OA.Domain.Services
+{
+    public interface IRewardService : IBaseService<Reward, RewardCreateVModel, RewardUpdateVModel, RewardGetByIdVModel, RewardGetAllVModel>
+    {
+        Task<ResponseResult> Search(RewardFilterVModel model);
+        Task<ExportStream> ExportFile(RewardFilterVModel model, ExportFileVModel exportModel);
+    }
+}

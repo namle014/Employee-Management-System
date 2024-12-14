b3bfd230aee6263835d89d835a3d496d1b3278ac
lexuannam26122004 <161745873+lexuannam26122004@users.noreply.github.com>
2024-12-14 22:06:16 +0700

    123

--- OA.Infrastructure.EF/Entities/AspNetUser.cs
+++ OA.Infrastructure.EF/Entities/AspNetUser.cs
@@ -12,7 +12,7 @@ namespace OA.Infrastructure.EF.Entities
     {
         public string FullName { get; set; } = string.Empty;
         public int? AvatarFileId { get; set; }
-        public bool Gender { get; set; }
+        public bool? Gender { get; set; }
         public string Address { get; set; } = string.Empty;
         public DateTime? Birthday { get; set; }
         public string JsonUserHasFunctions { get; set; } = string.Empty;
